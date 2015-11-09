51ce87a9a72b0fd05c17b6c4d3edf14614176f25
Nathan Hemingway <nhemingway@gmail.com>
2015-11-08 18:21:40 -0800

    Added commented out sync code

--- src/js/lib/pouchdb.js
+++ src/js/lib/pouchdb.js
@@ -2,4 +2,16 @@ import PouchDB from 'pouchdb'
 
 export const db = new PouchDB('main')
 
+PouchDB.debug.disable()
+
+/* Sync to a Couch instance
+const serverDB = PouchDB('...url...')
+
+
+db.sync(serverDB, {
+  live: true,
+  retry: true,
+})
+*/
+
 global.db = db
